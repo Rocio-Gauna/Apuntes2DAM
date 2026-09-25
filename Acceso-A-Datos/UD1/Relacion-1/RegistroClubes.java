bed51aca3b846830f343faef432033ebefe47a2d
Rocio-Gauna <rociogauna.3020@gmail.com>
2026-09-25 18:06:27 +0200

    terminar el ejercicio 8,

--- Acceso-A-Datos/UD1/Relacion-1/RegistroClubes.java
+++ Acceso-A-Datos/UD1/Relacion-1/RegistroClubes.java
@@ -1,4 +1,5 @@
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -7,13 +8,33 @@ import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class RegistroClubes {
-    public static void main(String[] args){
-      Path ruta = Path.of("datos", "clubes.txt");
-      
-      if (Files.notExists(ruta)){
-        
+    public static void main(String[] args) throws IOException {
 
-      }
+        Scanner scanner = new Scanner(System.in);
+    
+        Path carpeta = Path.of("datos");
+        Path archivoClubes = carpeta.resolve("clubes.txt");
+
+        if (Files.notExists(carpeta)){
+            Files.createDirectories(carpeta);
+        }
+        if (Files.notExists(archivoClubes)) {
+            Files.createFile(archivoClubes);
+        }
+
+        System.out.println("Ingrese el número de clubes a registrar:");
+        String id =scanner.next();
+
+        System.out.println("Ingrese nombre");
+        String nombre = scanner.next();
+
+        System.out.println("ingrese nombre de la ciudad");
+        String ciudad = scanner.next();
+        try (BufferedWriter salida = Files.newBufferedWriter(archivoClubes, StandardCharsets.UTF_8,){
+            
+        }
+
 
     }
+
 }
